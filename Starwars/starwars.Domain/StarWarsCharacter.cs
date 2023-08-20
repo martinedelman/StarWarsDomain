b83bea0108b04b9447c8f98951ba51166dd9d0ee
alexanderwieler <AW177322@fi365.ort.edu.uy>
2023-08-20 16:30:17 -0300

    new project

---
+++ Starwars/starwars.Domain/StarWarsCharacter.cs
@@ -0,0 +1,9 @@
+﻿namespace starwars.Domain;
+public class StarWarsCharacter
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+}
+
---
+++ Starwars/starwars.IBusinessLogic/IForceCalcStrategy.cs
@@ -0,0 +1,10 @@
+﻿using starwars.Domain;
+
+namespace starwars.IBusinessLogic;
+public interface IForceCalcStrategy
+{
+    int CalculateForce(IEnumerable<QuestionAnswer> answers);
+    StarWarsCharacter DetermineCharacter(int forceLevel);
+}
+
+
