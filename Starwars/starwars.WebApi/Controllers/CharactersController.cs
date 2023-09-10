905995460d51a0364eaac3e0a453affa1b1353ae
alexanderwieler <AW177322@fi365.ort.edu.uy>
2023-09-10 16:19:21 -0300

    TDD Webapi

---
+++ Starwars/starwars.WebApi/Controllers/CharactersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using starwars.IBusinessLogic;
+using starwars.WebApi.Dtos;
+
+namespace starwars.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CharactersController : ControllerBase
+{
+    private ICharacterService _characterService;
+
+    public CharactersController(ICharacterService characterService)
+    {
+        _characterService = characterService;
+    }
+
+    [HttpGet]
+    public IActionResult GetCharacters()
+    {
+        return Ok(_characterService.GetCharacters().Select(c => new CharacterDTO(c)).ToList());
+    }
+}
+
---
+++ Starwars/starwars.WebApi/Dtos/CharacterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using starwars.Domain;
+
+namespace starwars.WebApi.Dtos
+{
+	public class CharacterDTO
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public CharacterDTO(Character character)
+		{
+			Id = character.Id;
+			Name = character.Name;
+		}
+	}
+}
+
