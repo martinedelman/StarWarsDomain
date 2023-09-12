70f96fd1b7242356eecc8d084e6df379322dc016
alexanderwieler <AW177322@fi365.ort.edu.uy>
2023-09-12 09:32:48 -0300

    auth filter

---
+++ Starwars/starwars.WebApi/Filters/AuthorizationFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace starwars.WebApi.Filters
+{
+    public class AuthorizationFilter : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
