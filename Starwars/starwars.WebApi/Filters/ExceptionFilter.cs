87f56f08d5f3820d0927ab1bf975340ae07b6f6a
alexanderwieler <AW177322@fi365.ort.edu.uy>
2023-09-12 09:25:40 -0300

    Filters

---
+++ Starwars/starwars.WebApi/Filters/ExceptionFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using starwars.Exceptions.BusinessLogicExceptions;
+
+namespace starwars.WebApi.Filters
+{
+    public class ExceptionFilter : Attribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is InvalidField)
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 400,
+                    Content = context.Exception.Message,
+                };
+            }
+        }
+    }
+}
+
--- Starwars/starwars.WebApi/Program.cs
+++ Starwars/starwars.WebApi/Program.cs
@@ -1,4 +1,10 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using starwars.BusinessLogic;
+using starwars.DataAccess;
+using starwars.Domain;
+using starwars.IBusinessLogic;
+using starwars.IDataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +12,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<StarwarsContext>();
+
+builder.Services.AddScoped<ICharacterService, CharacterService>();
+builder.Services.AddScoped<IGenericRepository<Character>, CharacterManagment>();
+
 
 var app = builder.Build();
 
@@ -15,6 +27,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseAuthorization();
 
