c2dc7aa44614f36102e4037ab07230845d2fd4a9
martinedelman <martin@edelman.com.uy>
2023-10-10 20:12:41 -0300

    Create controller of characters

---
+++ Starwars/starwars.WebApi/Controllers/QuestionsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using starwars.IBusinessLogic;
+
+namespace starwars.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/questions")]
+    public class QuestionsController : ControllerBase
+    {
+        private IForceCalculator _forceCalculator;
+        public QuestionsController(IForceCalculator forceCalculator)
+        {
+            _forceCalculator = forceCalculator;
+        }
+
+        [HttpGet]
+        public IActionResult GetQuestions() {
+            throw new NotImplementedException();
+        }
+    }
+}
